a82898d31038678a2c32b66e7f025e2c5a8d699f
tallman9921 <yemelyan.yevdokimov.domain1121@gmail.com>
2022-01-24 19:44:31 +1000

    new

--- api/api_controller.js
+++ api/api_controller.js
@@ -21,74 +21,96 @@ module.exports = {
             direction: direction
         }
         try {
-            try {
-                await axios.post(
-                    process.env.PLATFORM_SERVER + "api/games/bet",
-                    {
-                        token: user[token].userToken,
-                        amount: user[token].betAmount,
-                    }
-                );
-            } catch (err) {
-                throw new Error("Bet Error!");
-            }
+            // try {
+            //     await axios.post(
+            //         process.env.PLATFORM_SERVER + "api/games/bet",
+            //         {
+            //             token: user[token].userToken,
+            //             amount: user[token].betAmount,
+            //         }
+            //     );
+            // } catch (err) {
+            //     throw new Error("Bet Error!");
+            // }
             var raiseprice;
             var dice = [];
             for (var i = 0; i < 2; i++) {
                 dice[i] = getRandomInt(6);
             }
             var sum = dice[0] + dice[1];
+            var cases = "";
+            //under
             if (user[token].direction == "True") {
-                if (user[token].sum >= sum) {
-                    if (sum == 2) {
-                        raiseprice = 0;
-                    } else if (sum == 3) {
+                if (user[token].sum > sum) {
+                    if (user[token].sum == 3) {
                         raiseprice = user[token].betAmount * 35.3;
-                    } else if (sum == 4) {
+                        cases = 5.3;
+                    } else if (user[token].sum == 4) {
                         raiseprice = user[token].betAmount * 11.8;
-                    } else if (sum == 5) {
+                        cases = 11.8;
+                    } else if (user[token].sum == 5) {
                         raiseprice = user[token].betAmount * 5.88;
-                    } else if (sum == 6) {
+                        cases = 5.88;
+                    } else if (user[token].sum == 6) {
                         raiseprice = user[token].betAmount * 3.53;
-                    } else if (sum == 7) {
+                        cases = 3.53;
+                    } else if (user[token].sum == 7) {
                         raiseprice = user[token].betAmount * 2.35;
-                    } else if (sum == 8) {
+                        cases = 2.35;
+                    } else if (user[token].sum == 8) {
                         raiseprice = user[token].betAmount * 1.68;
-                    } else if (sum == 9) {
+                        cases = 1.68;
+                    } else if (user[token].sum == 9) {
                         raiseprice = user[token].betAmount * 1.36;
-                    } else if (sum == 10) {
+                        cases = 1.36;
+                    } else if (user[token].sum == 10) {
                         raiseprice = user[token].betAmount * 1.18;
-                    } else if (sum == 11) {
+                        cases = 1.18;
+                    } else if (user[token].sum == 11) {
                         raiseprice = user[token].betAmount * 1.07;
-                    } else if (sum == 12) {
+                        cases = 1.07;
+                    } else if (user[token].sum == 12) {
                         raiseprice = user[token].betAmount * 1.01;
+                        cases = 1.01;
+                    } else {
+                        raiseprice = 0;
                     }
                 } else {
                     raiseprice = 0;
                 }
             } else {
-                if (user[token].sum <= sum) {
-                    if (sum == 2) {
+                if (user[token].sum < sum) {
+                    if (user[token].sum == 2) {
                         raiseprice = user[token].betAmount * 1.01;
-                    } else if (sum == 3) {
+                        cases = 1.01;
+                    } else if (user[token].sum == 3) {
                         raiseprice = user[token].betAmount * 1.07;
-                    } else if (sum == 4) {
+                        cases = 1.07;
+                    } else if (user[token].sum == 4) {
                         raiseprice = user[token].betAmount * 1.18;
-                    } else if (sum == 5) {
+                        cases = 1.18;
+                    } else if (user[token].sum == 5) {
                         raiseprice = user[token].betAmount * 1.36;
-                    } else if (sum == 6) {
+                        cases = 1.36;
+                    } else if (user[token].sum == 6) {
                         raiseprice = user[token].betAmount * 1.68;
-                    } else if (sum == 7) {
+                        cases = 1.68;
+                    } else if (user[token].sum == 7) {
                         raiseprice = user[token].betAmount * 2.35;
-                    } else if (sum == 8) {
+                        cases = 2.35;
+                    } else if (user[token].sum == 8) {
                         raiseprice = user[token].betAmount * 3.53;
-                    } else if (sum == 9) {
+                        cases = 3.53;
+                    } else if (user[token].sum == 9) {
                         raiseprice = user[token].betAmount * 5.88;
-                    } else if (sum == 10) {
+                        cases = 5.88;
+                    } else if (user[token].sum == 10) {
                         raiseprice = user[token].betAmount * 11.8;
-                    } else if (sum == 11) {
+                        cases = 11.8;
+                    } else if (user[token].sum == 11) {
                         raiseprice = user[token].betAmount * 35.3;
-                    } else if (sum == 12) {
+                        cases = 35.3;
+                    } else {
                         raiseprice = 0;
                     }
                 } else {
@@ -96,19 +118,24 @@ module.exports = {
                 }
             }
             var total = user[token].amount + raiseprice - user[token].betAmount;
-            var msg = "Price  : " + raiseprice.toFixed(3) + "$";
-            try {
-                await axios.post(
-                    process.env.PLATFORM_SERVER + "api/games/winlose",
-                    {
-                        token: user[token].userToken,
-                        amount: raiseprice,
-                        winState: raiseprice != 0 ? true : false,
-                    }
-                )
-            } catch (err) {
-                throw new Error("WinLose Error!");
+            var msg = "";
+            if (raiseprice == 0) {
+                msg = "You lose..";
+            } else {
+                msg = "Price  : " + "X" + cases + "$";
             }
+            // try {
+            //     await axios.post(
+            //         process.env.PLATFORM_SERVER + "api/games/winlose",
+            //         {
+            //             token: user[token].userToken,
+            //             amount: raiseprice,
+            //             winState: raiseprice != 0 ? true : false,
+            //         }
+            //     )
+            // } catch (err) {
+            //     throw new Error("WinLose Error!");
+            // }
             try {
                 res.json({
                     diceArray: dice,
